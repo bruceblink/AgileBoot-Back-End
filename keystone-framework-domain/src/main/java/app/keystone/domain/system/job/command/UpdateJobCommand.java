16aea4136c2bdd8cc49f4db1e90b222aa68837b8
bruceblink <likanug.g@qq.com>
2026-06-16 09:57:31 +0800

    fix(job): validate job URL identifiers

--- keystone-framework-domain/src/main/java/app/keystone/domain/system/job/command/UpdateJobCommand.java
+++ keystone-framework-domain/src/main/java/app/keystone/domain/system/job/command/UpdateJobCommand.java
@@ -1,7 +1,6 @@
 package app.keystone.domain.system.job.command;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -15,7 +14,6 @@ import lombok.EqualsAndHashCode;
 @Schema(description = "修改定时任务")
 public class UpdateJobCommand extends AddJobCommand {
 
-    @NotNull
     @Positive
     @Schema(description = "任务ID")
     private Long jobId;
